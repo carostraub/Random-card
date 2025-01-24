bc046ed0ddc663b136f4e7b6f693311558bb9754
caro.straub <carolina.straub@gmail.com>
2025-01-24 17:33:41 -0300

    cartarandom

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = ["♦", "♥", "♠", "♣"];
+  let cartas = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let carta = document.querySelector(".carta");
+  let pintas = document.getElementsByClassName("pinta");
+
+  let numeroRandom = Math.floor(Math.random() * cartas.length);
+  let pintaRandom = Math.floor(Math.random() * pinta.length);
+
+  console.log(pintaRandom + numeroRandom);
+
+  pintas.innerHTML = pinta[pintaRandom];
+  carta.innerHTML = cartas[numeroRandom];
+
+  for (let i = 0; i < pintas.length; i++) {
+    pintas[i].textContent = pinta[pintaRandom];
+    if (pinta[pintaRandom] === "♥" || pinta[pintaRandom] === "♦") {
+      pintas[i].style.color = "red";
+    } else {
+      pintas[i].style.color = "black";
+    }
+  }
 };
